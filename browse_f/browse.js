b9fa13f4db3adf33355940d8022cd5549f381bfe
Abhinav Sinha <abhinavraj350@gmail.com>
2022-10-17 15:57:19 +0530

    ketto.org

---
+++ browse_f/browse.js
@@ -0,0 +1,396 @@
+var fundData = [  
+    {
+        location: "Bengaluru",
+        type:    "Tax Benefit",
+        category: "All Categories",
+        img : "https://kettocdn.gumlet.io/media/campaign/180000/180773/image/wid6089ceb85c0bd.jpeg?dpr=1.0&q=70&w=300",
+        heading : "Feeding From Far-Ration Distribution Amid Lockdown",
+        logo : "https://kettocdn.gumlet.io/media/individual/1463000/1463388/image/62c32feb56031.jpg?dpr=1.0&q=70&w=50",
+        pName : "by Pooja Reddy",
+        amount : "₹ 4,80,99,363",
+        days : "152",
+        support :"12507",
+        
+    },
+    {
+        location: "New Delhi",
+        type: "Trending" ,
+        category: "All Categories",
+        img : "https://kettocdn.gumlet.io/media/campaign/212000/212655/image/wid1f1a33100b0ce11487774ca00c4341f7d0364e69.jpg?w=300&dpr=1.5",
+        heading : "My little boy’s cancer has relapsed twice but I’m...",
+        logo : "https://kettocdn.gumlet.io/media/individual/1463000/1463388/image/62c32feb56031.jpg?dpr=1.0&q=70&w=50",
+        pName : "by Family Friends",
+        amount : "₹ 4,05,72,702 ",
+        days : "15",
+        support :"20953",
+        
+    },
+    {
+        location: "Bengaluru",
+        type: "Trending" ,
+        category: "All Categories",
+        img : "https://kettocdn.gumlet.io/media/campaign/218000/218792/image/wid60326899153a5.jpeg?w=300&dpr=1.5",
+        heading : "Help father and his 20-year old son, Father-Kidney...",
+        logo : "https://kettocdn.gumlet.io/media/individual/1463000/1463388/image/62c32feb56031.jpg?dpr=1.0&q=70&w=50",
+        pName : "by Mansi Singh",
+        amount : "₹ 1,53,07,318",
+        days : "58",
+        support :"12400",
+        
+    },
+    {
+        location: "New Delhi",
+        type:    "Tax Benefit",
+        category: "Education",
+        img : "https://kettocdn.gumlet.io/media/campaign/277000/277698/image/wid5fa2de60a1557.jpeg?w=300&dpr=1.5",
+        heading : "Schooling Special needs Children with Custom...",
+        logo : "https://kettocdn.gumlet.io/media/ngo/2169000/2169187/image/607ea638e3612.jpg?w=50&dpr=1.5",
+        pName : "by Nithilyam Spastic Children...",
+        amount : "₹ 47,82,462",
+        days : "26",
+        support :"2458",
+        
+    },
+    {
+        location: "New Delhi",
+        type: "Trending" ,
+        category: "Education",
+        img : "https://kettocdn.gumlet.io/media/campaign/445000/445343/image/wid60f6ea7fc6789.jpeg?w=300&dpr=1.5",
+        heading : "BACK TO SCHOOL",
+        logo : "https://kettocdn.gumlet.io/media/ngo/4244000/4244769/image/b13b757146069d12464707fc1c77720aa9a495c5.jpg?w=50&dpr=1.5",
+        pName : "by Vidya Bhawan Society",
+        amount : "₹ 21,93,993 ",
+        days : "28",
+        support :"366",
+        
+    },
+    {
+        location: "Bengaluru",
+        type:    "Tax Benefit",
+        category: "Education",
+        img : "https://kettocdn.gumlet.io/media/campaign/621000/621515/image/wid6272c26d4eba8.png?w=300&dpr=1.5",
+        heading : "HELP WANGCHOK FROM LADAKH STUDY AT GENEVA",
+        logo : "https://kettocdn.gumlet.io/media/individual/6115000/6115567/image/6272c99719543.jpg?w=50&dpr=1.5",
+        pName : "by Wangchok Namgail",
+        amount : "₹ 7,37,198 ",
+        days : "7",
+        support :"459",
+        
+    },
+    {
+
+        location: "New Delhi",
+        type:    "Tax Benefit",
+        category: "Medical",
+        img : "https://kettocdn.gumlet.io/media/campaign/212000/212655/image/wid1f1a33100b0ce11487774ca00c4341f7d0364e69.jpg?w=300&dpr=1.5",
+        heading : "My little boy's cancer has relapsed twice but I'm...",
+        logo : "https://kettocdn.gumlet.io/media/individual/1463000/1463388/image/62c32feb56031.jpg?dpr=1.0&q=70&w=50",
+        pName : "by Family Friends",
+        amount : "₹ 4,05,72,702 ",
+        days : "15",
+        support :"20963",
+        
+    },
+    {
+        location: "Bengaluru",
+       
+        type: "Trending" ,
+        category:"Medical",
+        img : "https://kettocdn.gumlet.io/media/campaign/218000/218792/image/wid60326899153a5.jpeg?w=300&dpr=1.5",
+        heading : "Help father and his 20-year old son, Father-Kidney",
+        logo : "https://kettocdn.gumlet.io/media/individual/1825000/1825515/image/c245e813135d280903977246f606453166a2762c.jpeg?w=50&dpr=1.5",
+        pName : "by Mansi Singh",
+        amount : "₹ 1,53,07,318",
+        days : "58",
+        support :"12400",
+        
+    },
+    {
+       
+        location: "New Delhi",
+        type:    "Tax Benefit",
+        category:"Medical",
+        img : "https://kettocdn.gumlet.io/media/campaign/167000/167947/image/wid57beaf6d360986134ecdbd89657dd6a2b901a548.jpg?w=300&dpr=1.5",
+        heading : "Wife Fears She Will Lose The Father Of Their 2-Year-Old",
+        logo : "https://kettocdn.gumlet.io/media/individual/1463000/1463388/image/62c32feb56031.jpg?dpr=1.0&q=70&w=50",
+        pName : "by Rachit Shrivastava",
+        amount : "₹ 1,08,27,280",
+        days : "58",
+        support :"7002",
+        
+    },
+    {
+        location: "New Delhi",
+        type: "Trending" ,
+        category:"Women & Girls",
+        img : "https://kettocdn.gumlet.io/media/campaign/601000/601847/image/wid9b6ceae3e80f0c0e81a59ddf6224163eef3444b1.jpg?w=300&dpr=1.5",
+        heading : "Scarred but alive - support acid attack survivors by...",
+        logo : "https://kettocdn.gumlet.io/media/individual/1463000/1463388/image/62c32feb56031.jpg?dpr=1.0&q=70&w=50",
+        pName : "by Chhanv Foundation",
+        amount : "₹ 7,46,683 ",
+        days : "9",
+        support :"148",
+        
+    },
+    {   
+        location: "Bengaluru",
+        type: "Trending" ,
+        category:"Women & Girls",
+        img : "https://kettocdn.gumlet.io/media/campaign/117000/117114/image/wid5eba67d0e4594.jpeg?w=300&dpr=1.5",
+        heading : "Homeless animals need your help, please donate as much...",
+        logo : "https://kettocdn.gumlet.io/media/individual/1019000/1019949/image/5f19cbbdac680d5018dd43bae2870f48bf99097b.jpg?w=50&dpr=1.5",
+        pName : "by vandana (Guddee) Maida",
+        amount : "₹ 2,29,698",
+        days : "28",
+        support :"175",
+        
+    },
+    {
+        location: "New Delhi",
+        type:    "Tax Benefit",
+        category:"Women & Girls",
+        img : "https://kettocdn.gumlet.io/media/campaign/209000/209801/image/wid5ef0926136ba7.png?w=300&dpr=1.5",
+        heading : "Help underprivileged women with sanitary pads",
+        logo : "https://kettocdn.gumlet.io/media/individual/1721000/1721508/image/5f0032b1964b0.jpg?w=50&dpr=1.5",
+        pName : "by Niveda ravikumar",
+        amount : "₹ 4,38,511 ",
+        days : "52",
+        support :"507",
+        
+    },
+    {
+        location: "Bengaluru",
+        type:    "Tax Benefit",
+        type: "Trending" ,
+        category:"Animals",
+        img : "https://kettocdn.gumlet.io/media/campaign/522000/522460/image/wid6167f51f7a909.jpeg?w=300&dpr=1.5",
+        heading : "URGENT HELP needed for our shelter and our strays",
+        logo : "https://kettocdn.gumlet.io/media/ngo/119000/119583/image/04b7395a816c9206a9129e7cb33dff0ac41117d8.jpg?w=50&dpr=1.5",
+        pName : "by People For Animals Agra",
+        amount : "₹ 11,27,968",
+        days : "28",
+        support :"226",
+        
+    },
+    {
+        location: "New Delhi",
+        type:    "Tax Benefit",
+        category: "Animals",
+        img : "https://kettocdn.gumlet.io/media/campaign/489000/489376/image/wid62da8c9571859.jpg?w=300&dpr=1.5",
+        heading : "Grandpa Bob's Ray of Hope",
+        logo : "https://kettocdn.gumlet.io/media/individual/4882000/4882249/image/62d1ad009fc28.jpg?w=50&dpr=1.5",
+        pName : "by Sushama C.",
+        amount : "₹ 8,44,870 ",
+        days : "28",
+        support :"330",
+        
+    },
+    {
+        location: "New Delhi",
+        type: "Trending" ,
+        category:"Animals",
+        img : "https://kettocdn.gumlet.io/media/campaign/389000/389487/image/wid609281c402715.jpeg?w=300&dpr=1.5",
+        heading : "Help Rescue & Feed The Strays In The Pandemic",
+        logo : "https://kettocdn.gumlet.io/media/ngo/4096000/4096939/image/61506cfe0cb33.jpg?w=50&dpr=1.5",
+        pName : "by TOUCH & TREAT ANIMAL TRUS",
+        amount : "₹ 2,70,171 ",
+        days : "78",
+        support :"126",
+        
+    },
+    {
+        location: "Bengaluru",
+        type:    "Tax Benefit",
+        category:"Creative",
+        img : "https://kettocdn.gumlet.io/media/campaign/159000/159901/image/wid14d0dd18e576af46ff5a2373936e8d42cb9f42df.png?w=300&dpr=1.5",
+        heading : "Help Us to Plant 1 Trillion Tree Worldwide",
+        logo : "https://kettocdn.gumlet.io/media/ngo/4096000/4096939/image/61506cfe0cb33.jpg?w=50&dpr=1.5",
+        pName : " by Planet Voice Foundation",
+        amount : "₹ 8,14,235  ",
+        days : "28",
+        support :"726",
+        
+    },
+    {
+        location: "Bengaluru",
+        type: "Trending" ,
+        category:"Creative",
+        img : "https://kettocdn.gumlet.io/media/campaign/83000/83346/image/wid5c6fb27cedf3e.jpeg?w=300&dpr=1.5",
+        heading : "Darya Firasti II",
+        logo : "https://kettocdn.gumlet.io/media/individual/56000/56695/image/6278916f5607a.jpg?w=50&dpr=1.5",
+        pName : " by Chinmaye Bhavé",
+        amount : "₹ 4,79,900 ",
+        days : "57",
+        support :"117",
+        
+    },
+    {  
+        location: "Bengaluru",
+        type:    "Tax Benefit",
+        category:"Creative",
+        img : "https://kettocdn.gumlet.io/media/campaign/607000/607589/image/wid624a99cecbc0a.jpeg?w=300&dpr=1.5",
+        heading : "Help IISER Pune students make it to iGEM 2022!",
+        logo : "https://kettocdn.gumlet.io/media/individual/5987000/5987213/image/62a417d8c8f78.jpg?w=50&dpr=1.5",
+        pName : " by Shreya Kulkarni",
+        amount : "₹ 1,76,575  ",
+        days : "22",
+        support :"82",
+        
+    },
+    {
+        location: "New Delhi",
+        type: "Trending" ,
+        category:"Creative",
+        img : "https://kettocdn.gumlet.io/media/campaign/656000/656006/image/wid62ea5d2df1f1f.jpg?w=300&dpr=1.5",
+        heading : "Help IISER Pune students make it to iGEM 2022!",
+        logo : "https://kettocdn.gumlet.io/media/individual/6292000/6292724/image/62d5957e8287d.jpg?w=50&dpr=1.5",
+        pName : "by Sumi Mathai",
+        amount : "₹ 1,62,850 ",
+        days : "28",
+        support :"41",
+        
+    },
+ ];
+
+ var fundfilter = JSON.parse(localStorage.getItem("Window")) || [];
+   
+
+ displayData(fundData);
+function displayData(fundData){
+    document.getElementById("data").innerText ="";
+    fundData.map(function(elem,index){
+  var div = document.createElement("div");
+  div.setAttribute("id","parentBox");
+  var image = document.createElement("img");
+   image.setAttribute("id","avtar")
+   image.setAttribute("src", elem.img)
+   image.setAttribute("alt",index);
+  var h4 = document.createElement("h4");
+  h4.innerText = elem.heading;
+//   h4.setAttribute("id", "h4");
+  var smallDiv = document.createElement("div");
+    smallDiv.setAttribute("id", "logo")
+  var logo = document.createElement("img");
+    logo.setAttribute("id","imgLogo")
+   logo.setAttribute("src", elem.logo);
+   logo.setAttribute("alt", index);
+
+   var pName = document.createElement("p");
+   pName.innerText = elem.pName;
+
+    var amountDiv = document.createElement("div");
+    amountDiv.setAttribute("id","amount");
+    var amount = document.createElement("h3")
+     amount.innerText = elem.amount;
+    
+     var raised = document.createElement("p");
+     raised .innerText = "raised";
+     raised.setAttribute("id", "raised");
+
+     var hr = document.createElement("hr");
+     hr.setAttribute("id", "hr");
+     
+     var pLast = document.createElement("p");
+     pLast.innerText = "Last donation an hours ago";
+     pLast.setAttribute("id", "pLast");
+  
+     var newDiv = document.createElement("div");
+     newDiv.setAttribute("id", "newDiv");
+
+      var dayDiv = document.createElement("div");
+      dayDiv.setAttribute("id","day")
+      var day = document.createElement("h3");
+      day.innerText = elem.days;
+
+       var dayLeft = document.createElement("p");
+       dayLeft.innerText ="Day Left";
+       
+       var supportersDiv = document.createElement("div");
+       supportersDiv.setAttribute("id", "supporters");
+       var supporters = document.createElement("h3");
+       supporters.innerText = elem.support;
+
+        var pSupporters = document.createElement("p")
+        pSupporters.innerText = "Supporters"
+
+        newDiv.append(dayDiv,supportersDiv);
+        supportersDiv.append(supporters, pSupporters);
+        dayDiv.append(day,dayLeft);
+        amountDiv.append(amount, raised);
+        smallDiv.append(logo,pName);
+         
+        div.append(image, h4, smallDiv, amountDiv, hr, pLast, newDiv);
+
+        document.getElementById("data").append(div);
+
+    });
+}
+
+
+
+function handleRoleFilter(){
+var Category = document.getElementById("filterRole").value;
+
+var filterEd = fundData.filter(function(elem){
+if(Category === "All Categories" || Category === ""){
+ return 1;
+} else{
+ return elem.category== Category;
+      }
+
+ })
+    console.log(filterEd);
+    displayData(filterEd);
+    
+   }
+
+   function typeFilter(){
+    var TT = document.getElementById("fund").value;
+   
+var filterType = fundData.filter(function(elem){
+     if(TT === "All Types" || TT === ""){
+        return 1;
+     }else{
+        return elem.type == TT;
+        
+     }
+     })
+     console.log(filterType);
+    displayData(filterType);
+}
+
+
+function locationSearch(){
+    var Sl = document.getElementById("location").value;
+ 
+    var filterLocation = fundData.filter(function(elem){
+        if(Sl === "All Location" || Sl === "" ){
+            return 1;
+        }else{
+            return elem.location == Sl;
+            
+        }
+    })
+    console.log(filterLocation);
+    displayData(filterLocation);
+}
+
+
+ 
+function showEducation(){
+    var EDu=document.getElementById("Educaion").value;
+    var filterEDu = fundData.filter(function(elem){
+        if(EDu === "Education"){
+            return elem.category==filterEDu;
+        }
+    })
+    displayData(filterEDu);
+    console.log(filterEDu);
+    
+}
+
+
+
+ 
+
+
